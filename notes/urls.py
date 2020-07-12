beec86af6bf2b1dba8a9901a7cf6702e15dc7532
João Paulo Zorek <joaopaulozorek@gmail.com>
2020-07-12 20:45:26 -0300

    Adicionei as views para criar/editar a nota

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.notes_list, name='notes_list'),
 	path('note/<int:pk>/', views.note_detail, name='note_detail'),
+	path('note/new/', views.note_new, name='note_new'),
+	path('note/<int:pk>/edit/', views.note_edit, name='note_edit'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import NoteForm
 from .models import Note
 
 
@@ -10,3 +11,30 @@ def notes_list(request):
 def note_detail(request, pk):
 	note = get_object_or_404(Note, pk=pk)
 	return render(request, 'notes/note_detail.html', {'note': note})
+
+def note_new(request):
+	if request.method == "POST":
+		form = NoteForm(request.POST)
+		if form.is_valid():
+			note = form.save(commit=False)
+			note.author = request.user
+			note.date = timezone.now()
+			note.save()
+			return redirect('note_detail', pk=note.pk)
+	else:
+		form = NoteForm()
+	return render(request, 'notes/note_edit.html', {'form': form})
+
+def note_edit(request, pk):
+	note = get_object_or_404(Note, pk=pk)
+	if request.method == "POST":
+		form = NoteForm(request.POST, instance=note)
+		if form.is_valid():
+			note = form.save(commit=False)
+			note.author = request.user
+			note.date = timezone.now()
+			note.save()
+			return redirect('note_detail', pk=note.pk)
+	else:
+		form = NoteForm(instance=note)
+	return render(request, 'notes/note_edit.html', {'form': form})
