71f2e7dc9fe00adcfbba6517891960e8ca0edf1e
João Paulo Zorek <joaopaulozorek@gmail.com>
2020-07-11 23:45:05 -0300

    Adicionei o template de detalhe da Nota

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.notes_list, name='notes_list'),
+	path('note/<int:pk>/', views.note_detail, name='note_detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Note
 
 
 def notes_list(request):
 	notes = Note.objects.filter(date__lte=timezone.now()).order_by('date') 
-	return render(request, 'notes/notes_list.html', {'notes': notes})
+	return render(request, 'notes/note_list.html', {'notes': notes})
+
+def note_detail(request, pk):
+	note = get_object_or_404(Note, pk=pk)
+	return render(request, 'notes/note_detail.html', {'note': note})
