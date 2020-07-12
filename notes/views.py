29e57b3b4521696e6b6ab24530945392749009d6
João Paulo Zorek <joaopaulozorek@gmail.com>
2020-07-11 22:52:48 -0300

    Exibindo as notas no template

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Note
 
 
 def notes_list(request):
-
-	return render(request, 'notes/notes_list.html', {})
+	notes = Note.objects.filter(date__lte=timezone.now()).order_by('date') 
+	return render(request, 'notes/notes_list.html', {'notes': notes})
